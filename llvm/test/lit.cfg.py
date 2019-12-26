f9ba3becc7f1c24f418c84321d7fd5c42b435e1a
Marc Jacobi <marc.jacobi@macaw.nl>
2019-12-26 10:44:47 +0100

    typo

--- llvm/test/lit.cfg.py
+++ llvm/test/lit.cfg.py
@@ -20,7 +20,7 @@ config.name = 'LLVM'
 # testFormat: The test format to use to interpret tests.
 config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
 
-# suffixes: A list of file extensions to treat as test files. This is overriden
+# suffixes: A list of file extensions to treat as test files. This is overridden
 # by individual lit.local.cfg files in the test subdirectories.
 config.suffixes = ['.ll', '.c', '.cxx', '.test', '.txt', '.s', '.mir']
 
@@ -172,7 +172,6 @@ tools.extend([
 llvm_config.add_tool_substitutions(tools, config.llvm_tools_dir)
 
 # Targets
-
 config.targets = frozenset(config.targets_to_build.split())
 
 for arch in config.targets_to_build.split():
